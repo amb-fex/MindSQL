e03d88adc12ab9870b29fcaebbcd1c9f89028fdd
amb-fex <sdefex@amb.cat>
2025-02-22 15:45:37 +0100

    Update __init__.py

--- mindsql/databases/__init__.py
+++ mindsql/databases/__init__.py
@@ -2,12 +2,5 @@ from .idatabase import IDatabase
 from .mysql import MySql
 from .postgres import Postgres
 from .sqlite import Sqlite
-from .sqlserver import SQLServer
 
-# Conditionally import SQLServer
-try:
-    from .sqlserver import SQLServer
-except ImportError:
-    # Skip if pyodbc is not installed
-    SQLServer = None
 
